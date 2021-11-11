ae9247722eab77b85167686e38b406ebf2a77c77
momoladebrouill <momoladebrouill@gmail.com>
2021-11-11 19:47:19 +0100

    en fait maintenant c'est un simulateur de colisions

--- pingpong.py
+++ pingpong.py
@@ -1,82 +1,100 @@
 import pygame as pg
 import utis
+from random import random
 from utis import math
 
 pg.init()
 X=750
 Y=500
-f=pg.display.set_mode(size=(X,Y))
+f=pg.display.set_mode(size=(X+50,Y+50))
 fps=pg.time.Clock()
 B=1
-class balle:
-    x=X/2
-    y=Y/2
-    ray=5
-    vec=utis.Vec(long=5,angle=math.tau/6)
-    rect=pg.draw.circle(f,0xffffff,(x,y),ray)
-    def frwd():
-        balle.x+=balle.vec.x
-        balle.y+=balle.vec.y
-    def mv():
-        balle.frwd()
-        balle.draw()
-        palets=[gauche,droite]
-        ind=balle.rect.collidelist(palets)
-        if ind!=-1:
-            truc=palets[ind]
-            if balle.y<truc.y:
-                balle.vec.collide(math.pi/2)
-                balle.frwd()
-            elif balle.y>truc.y:
-                balle.vec.collide(math.pi/2)
-                balle.frwd()
-            else:
-                balle.vec.collide(math.pi)
-                balle.frwd()
-            
-            
-        if balle.x>X or balle.x<0:
-            balle.vec.collide(math.pi/2)
-            balle.frwd()
-        if balle.y>Y or balle.y<0:
-            balle.vec.collide(math.pi)
-            balle.frwd()
-    def draw():
-        balle.rect=pg.draw.circle(f,0xffffff,(balle.x,balle.y),balle.ray)
-
-class Pal:
-    def __init__(self):
-        self.x=10
-        self.y=Y/2
-        self.rect=pg.Rect(self.x,self.y,10,50)
+ATTRACTION=.5
+class Balle:
+    def __init__(self,X,Y):
+        self.x=X
+        self.y=Y
+        self.speed=2.5
+        self.vecx=0
+        self.vecy=0
+        self.ray=10
+        self.coul=int(random()*0xffffff)
+        self.rect=pg.draw.circle(f,self.coul,(self.x,self.y),self.ray)
+        
+        
+    def move(self):
+        self.x+=self.vecx
+        self.y+=self.vecy
+        
     def draw(self):
-        self.rect=pg.draw.rect(f,0xff0000,(self.x,self.y,10,50))
+        self.rect=pg.draw.circle(f,self.coul,(self.x,self.y),self.ray)
+       
+        
+    def collide(self):
+        if self.x>X:
+            self.vecx=X-self.x
+        elif balle.y>Y:
+            self.vecy=Y-self.y
+        elif self.x<0:
+            self.vecx=-self.x-2
+        elif self.y<0:
+            self.vecy=-self.y
+        else:
+            ls=[]
+            for obj in balls:
+                if not obj is self:
+                    ls.append(obj)
+            who=self.rect.collidelist(ls)
+     
+            
+            if who!=-1:
+                other=balls[who]
+                self.vecy=self.y-other.y
+                self.vecx=self.x-other.x
+                if self.vecx==self.vecy==0:
+                    self.vecx=random()
+                    self.vecy=random()
+        self.vecy+=.01
+        self.vecx-=math.copysign(.01,self.x-X/2)
+        self.vecx+=(sum(obj.x for obj in balls)/len(balls)-self.x)*ATTRACTION
+        self.vecy+=(sum(obj.y for obj in balls)/len(balls)-self.y)*ATTRACTION
+    __repr__=lambda s:f'|{s.x},{s.y}|'
+    
+
+
 pres={}
-gauche=Pal()
-droite=Pal()
-droite.x=X-gauche.x-10
+def move_all(x=0,y=0):
+    for balle in balls:
+        balle.x+=x*10
+        balle.y+=y*10
+mousepos=(50,50)
+balls=[Balle(X*random(),Y*random()) for _ in range(3)]
 while B:
     fps.tick(60)
     pg.display.flip()
     f.fill(0)
-    balle.mv()
-    balle.draw()
-    gauche.draw()
-    droite.draw()
-    if pres.get(pg.K_a):
-        gauche.y-=10
-    elif pres.get(pg.K_q):
-        gauche.y+=10
-    if pres.get(pg.K_p):
-        droite.y-=10
-    elif pres.get(pg.K_m):
-        droite.y+=10
-    droite.y=droite.y%Y
-    gauche.y=gauche.y%Y
+    for balle in balls:
+        balle.move()
+        balle.draw()
+        balle.collide()
+        
+    if pres.get(pg.K_z):
+        move_all(0,-1)
+        
+    if pres.get(pg.K_q):
+        move_all(-1)
+        
+    if pres.get(pg.K_s):
+        move_all(0,1)
+    if pres.get(pg.K_d):
+        move_all(1)
+        
     for event in pg.event.get():
         if event.type==pg.QUIT:
             B=0
             pg.quit()
+        elif event.type==pg.MOUSEBUTTONUP:
+            balls.append(Balle(*event.pos))
         elif event.type==pg.KEYUP:
             pres[event.dict['key']]=False
         elif event.type==pg.KEYDOWN:
